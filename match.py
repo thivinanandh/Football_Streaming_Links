8ab1e1b0e686e885c16b281692fd93884c636280
thivinanandh <thivinanandh@gmail.com>
2022-08-29 14:58:15 +0530

    Added enw logic for getting links and storing them as DF

--- match.py
+++ match.py
@@ -1,31 +1,53 @@
-
+# %%
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
+import os
+from pprint import pprint
+import re
  
-url = 'http://www.redditsoccerstreams.tv/'
+url = 'http://www.redditsoccerstreams.tv'
 reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 
-Matches = []
-Links = []
-for link in soup.find_all('td',class_="et3"):
-    htmlval = link.find_next_siblings('td')
-    soupMatch = BeautifulSoup(str(htmlval[0]),features="html.parser")
-    soupLink = BeautifulSoup(str(htmlval[1]),features="html.parser")
-    if(soup.get_text() and len(soupLink.find_all('a', href=True))):
-        Matches.append(soupMatch.get_text().lower())
-        UrlValue = soupLink.find_all('a', href=True)[0]['href']
-        reqs = requests.get(UrlValue)
-        soup = BeautifulSoup(reqs.text, 'html.parser')
-        if(len(soup.find_all('iframe'))):
-            Links.append(soup.find_all('iframe')[0]['src'])
-        
-dataFrame = pd.DataFrame([Matches,Links],).T
-dataFrame.columns = ["Match","Link"]
 
-from jinja2 import Environment, FileSystemLoader
-import os
+## obtain all the links from the given URL and save it as a DF
+table = soup.find( "table" )
+
+## replace Watch with href Links 
+regex = '<td><a href=\"(.*?)\">.*?<\/a><\/td>'
+subst = '<td>\\1</td>'
+tableNew = re.sub(regex,subst,str(table),re.MULTILINE)
+tableDF = pd.read_html(str(tableNew))[0]
+c = ["Time","Match","Link"]
+tableDF.columns =c 
+# %%
+
+## this function identifies, if there is an iframe present within the
+## given url or not. 
+def iframeIdentifier(url):
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    if(len(soup.find_all('iframe'))):
+        return soup.find_all('iframe')[0]['src']
+    else:
+        return "IframenotFound"
+
+
+for  rowIndex in range(tableDF.shape[0]):
+    row = tableDF.iloc[rowIndex,:]
+    oldLink = row["Link"]
+    if("http:" not in oldLink):
+        continue
+    newLink = iframeIdentifier(oldLink)
+    if(newLink != "IframenotFound"):
+        row["Link"] = newLink
+
+
+
+# %%
+tableDF_valid = tableDF[tableDF["Link"].str.contains("http") ]
 templateLoader = FileSystemLoader(searchpath="./")
 templateEnv = Environment(loader=templateLoader)
 template = templateEnv.get_template('template.html')
@@ -35,8 +57,9 @@ with open(filename, 'w') as fh:
         my_string = "Hello Jinja2",
         show_one = True,
         show_two = False,
-        my_list    = dataFrame['Link'].to_list(),
-        names = dataFrame['Match'].to_list(),
+        my_list    = tableDF_valid['Link'].to_list(),
+        names = tableDF_valid['Match'].to_list(),
         zip=zip
     ))
 print("Match link generation completed")
+# %%
